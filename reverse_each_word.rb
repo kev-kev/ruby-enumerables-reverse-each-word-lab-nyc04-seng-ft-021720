28e4e3cf9e71df35ad648b76482a781fd253a5cc
Kevin Clark <kevin.clark93@gmail.com>
2020-02-10 17:58:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
 def reverse_each_word(str)
+  # Solving without using collect
+  
   # result = []
   # str.split.each{ |word|
   #   result.push(word.reverse)
   # }
   # result.join(" ")
+  
+  # Solving with collect
   
 end
 
